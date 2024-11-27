5a5c9d9bd56cf47906a953507a26dfc07e9653fc
aegroto <aegroto@protonmail.com>
2024-11-27 15:06:09 +0100

    added compression and subsampling scripts

--- run.sh
+++ run.sh
@@ -3,3 +3,12 @@ ffmpeg -y -i music_video.mp4 -vn -c:a flac whole_audio.flac
 
 echo "Slice an audio"
 ffmpeg -y -ss 00:00:11 -to 00:00:16 -i whole_audio.flac -c copy audio_section.flac
+
+echo "Lossy MP3 compression"
+ffmpeg -y -i audio_section.flac -c:a libmp3lame audio_section.mp3
+
+echo "Subsampling at 24k"
+ffmpeg -y -i audio_section.flac -ar 24k audio_section_24k.flac
+
+echo "Subsampling at 12k"
+ffmpeg -y -i audio_section.flac -ar 12k audio_section_12k.flac
