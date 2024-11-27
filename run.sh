68f64d382f03058b18c52b2c90dd270eadef56d8
aegroto <aegroto@protonmail.com>
2024-11-27 16:02:21 +0100

    added channel split example

--- run.sh
+++ run.sh
@@ -12,3 +12,9 @@ ffmpeg -y -i audio_section.flac -ar 24k audio_section_24k.flac
 
 echo "Subsampling at 12k"
 ffmpeg -y -i audio_section.flac -ar 12k audio_section_12k.flac
+
+echo "Extract left channel"
+ffmpeg -y -i audio_section.flac \
+    -filter_complex "[0:a]channelsplit=channel_layout=stereo[left][right]" \
+    -map "[left]" audio_section_left.flac \
+    -map "[right]" audio_section_right.flac
